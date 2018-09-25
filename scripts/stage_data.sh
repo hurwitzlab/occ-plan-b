c08c6f7dd9c4521ccb00ae8e4ccc139bf64e96f9
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-09-25 11:42:07 -0700

    Decompress inputs instead of slow conversion to bzip2

--- scripts/stage_data.sh
+++ scripts/stage_data.sh
@@ -22,22 +22,29 @@ echo $filelist
 
 for f in $filelist
 do
-    echo $IRODS_PATH/$f
-
+    echo "Started transferring" $f
     iget -PTf $IRODS_PATH/$f $STAGING_PATH
+    echo "Finished transferring" $f
 
     # Run Illyoung's hashing script -- temporary addition for his thesis
+    echo "Hashing" $f
     /home/mbomhoff/bin/hash_blocks.py $STAGING_PATH/$f || true
 
     ext="${f##*.}"
     name="${f%.*}"
 
     if [[ "$ext" = "gz" || "$ext" = "gzip" ]]; then
-        gunzip --stdout $STAGING_PATH/$f | bzip2 > $STAGING_PATH/$name.bz2
-        rm $STAGING_PATH/$f
-        f=$name.bz2
+        # mdb removed 9/25/18 -- too slow to convert to bzip2, just decompress 
+        #echo "Converting to bzip2" $f
+        #gunzip --stdout $STAGING_PATH/$f | /home/mbomhoff/tmp/pbzip2-1.1.8/pbzip2 > $STAGING_PATH/$name.bz2
+
+        echo "Decompressing" $f
+        gunzip $STAGING_PATH/$f
+        #rm $STAGING_PATH/$f
+        f=$name
     fi
 
+    echo "Copying to HDFS" $f
     hdfs dfs -put -f $STAGING_PATH/$f $HDFS_PATH
 
     rm $STAGING_PATH/*
